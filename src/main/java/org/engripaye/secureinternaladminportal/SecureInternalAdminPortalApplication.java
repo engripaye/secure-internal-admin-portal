380247d4e637db59cb4816c2e0acf639501d985d
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-08-06 00:48:31 +0100

    first commit by ENGR. IPAYE

---
+++ src/main/java/org/engripaye/secureinternaladminportal/SecureInternalAdminPortalApplication.java
@@ -0,0 +1,13 @@
+package org.engripaye.secureinternaladminportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureInternalAdminPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureInternalAdminPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/engripaye/secureinternaladminportal/SecureInternalAdminPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.engripaye.secureinternaladminportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureInternalAdminPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
