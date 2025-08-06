636c24308bc29b3aab9e0e13183bd5dcf29bea56
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-08-06 08:11:11 +0100

    yaml commit by ENGR. IPAYE

--- src/main/java/org/engripaye/secureinternaladminportal/SecureInternalAdminPortalApplication.java
+++ src/main/java/org/engripaye/secureinternaladminportal/SecureInternalAdminPortalApplication.java
@@ -2,12 +2,13 @@ package org.engripaye.secureinternaladminportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:application-secret.yaml", factory = org.engripaye.secureinternaladminportal.YamlPropertySourceFactory.class)
 public class SecureInternalAdminPortalApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SecureInternalAdminPortalApplication.class, args);
     }
-
 }
+
